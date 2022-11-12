68437370162a9eec5b321da3fa17f56f013b3e50
FluffyCube9343 <learningninja007@gmail.com>
2022-11-12 15:45:52 -0500

    carry over chanegs to other boxes

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 
 from flask import Flask, render_template, request
 from flask_assets import Bundle, Environment
-import jyserver.Flask as jsf
 
 app = Flask(__name__)
 
@@ -12,20 +11,10 @@ css = Bundle("src/main.css", output="dist/main.css")
 assets.register("css", css)
 css.build()
 
-@jsf.use(app)
-class App:
-    def __init__(self) -> None:
-        pass
-
-    def printinput(self):
-        x = self.js.document.getElementById('userInput').value
-        print(x)
-
 
 @app.route("/")
 def homepage():
-
-    return App.render(render_template("index.html"))
+    return render_template("index.html")
 
 @app.route("/base.html", methods =["GET", "POST"])
 def sendData():
