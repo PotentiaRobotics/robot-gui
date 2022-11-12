8b0248320d51fed32763662ffaca897fed271e8e
FluffyCube9343 <learningninja007@gmail.com>
2022-11-12 15:34:19 -0500

    basic comms to raspberry pi w/confirmation

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # app.py
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_assets import Bundle, Environment
 
 app = Flask(__name__)
@@ -16,6 +16,22 @@ css.build()
 def homepage():
     return render_template("index.html")
 
+@app.route("/base.html", methods =["GET", "POST"])
+def sendData():
+            
+    rot = request.form.get("rot")
+
+    print("Activated")
+    import socket
+    s=socket.socket()
+    host="raspberrypi"       #This is your Server IP!
+    port=2345
+    s.connect((host,port))
+    s.send(str(rot).encode())
+    rece=s.recv(1024)
+    print("Received",rece)
+    s.close()
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
